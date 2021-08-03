aa42f7ded37f112f9a737b339f4cdb6c0f765a70
Andrew McDOnald <47397734+bfhobbes@users.noreply.github.com>
2021-08-03 00:28:39 -0700

    Setings working - although eeprom write is failing.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_EEPROM_I2C.h>
 
 #include <Bounce2.h>
 
@@ -16,6 +17,8 @@
 #define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+Adafruit_EEPROM_I2C i2ceeprom;
+
 #define FLASH_PIN LED_BUILTIN
 #define ARMED_LIGHT  7
 #define ARMING_PIN 9
@@ -23,11 +26,13 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define RELAY_PIN 10
 
 enum Events {
-  ARMING_DOWN = 0,
-  ARMING_UP,
-  LAUNCH_DOWN,
-  LAUNCH_UP,
+  SAFETY_OFF,
+  SAFETY_ON,
+  LAUNCH_ON,
+  LAUNCH_OFF,
   ENTER_SETTING,
+  ENTER_SETTING_VALUE,
+  EXIT_SETTING,
   ENTER_ARMED, 
   LAUNCH
 };
@@ -41,34 +46,33 @@ enum SettingName {
   VALVE_TIME,
   LAUNCH_MODE,
   COUNTDOWN_TIMER,
-  AUDIO
+  AUDIO,
+  SETTING_MAX
 };
+
+
 
 static const SettingInfo settings[] = {
   {
-    "Option1", { "o1_val1", "o1_val2", nullptr }
+    "Valve Time", { "1000ms", "750ms", "500ms", "300ms", nullptr }
   },
   {
-    "Option2", { "o2_val1", "o2_val2", "o2_val3", nullptr }
+    "Lnch Mode", { "Instant", "Count",  nullptr }
   },
   {
-    "Option3", { "o3_val1", "o3_val2", "o3_val3", "o3_val4", nullptr }
+    "Cnt Timer", { "10", "5", "3", nullptr }
   },
   {
     "Exit", { nullptr }
   }
 };
 
+uint8_t settingValues[SETTING_MAX];
+uint8_t pendingValues[SETTING_MAX];
+
 int currentSetting = 0;
-int currentValue = 0;
 
 const int NUM_SETTINGS = sizeof(settings)/sizeof(SettingInfo);
-int settingVals[NUM_SETTINGS];
-
-void testdrawline();
-void testdrawchar();
-void testdrawstyles();
-void testscrolltext();
 
 void showText(const arduino::__FlashStringHelper *text) {
   display.clearDisplay();
@@ -109,6 +113,12 @@ void showText3(const arduino::__FlashStringHelper *text1, const arduino::__Flash
     display.println(text3);
   }
   display.display();      // Show initial text
+}
+
+int getNumberValue(int setting) {
+  int val = 0;
+  sscanf(settings[setting].values[settingValues[setting]], "%d", &val);
+  return val;
 }
 
 Bounce safetySwitch;
@@ -185,9 +195,7 @@ void on_prearm_enter(void *) {
 }
 
 void on_prearm(void *) {
-  if(!launchSwitch.read()) {
-    mainFsm.trigger(ENTER_SETTING);
-  } else {
+  if(launchSwitch.read()) {
     mainFsm.trigger(ENTER_ARMED);
   }
 }
@@ -212,8 +220,10 @@ void on_settingname_enter(void *) {
 }
 
 void on_settingvalue_enter(void *) {
+  uint8_t currentValue = pendingValues[currentSetting];
   if(settings[currentSetting].values[currentValue] == nullptr) {
     currentValue = 0;
+    pendingValues[currentSetting] = 0;
   }
   showText3(
     F("Setting"),
@@ -222,44 +232,118 @@ void on_settingvalue_enter(void *) {
   );
 }
 
-int countdownStart = 0;
+int countdownEnd = 0;
 void on_countdown_enter(void *) {
-  countdownStart = millis();
+  countdownEnd = millis() + (1000 * getNumberValue(COUNTDOWN_TIMER));
 }
 
 void on_countdown_update(void *) {
-  int countdown = 10000 + countdownStart - millis();
+  int countdown = countdownEnd - millis();
   
   char buff[20];
   sprintf(buff, "%.2f", (float)countdown / 1000.0f);
 
   showCountdownText(F(buff));
+  if(safetySwitch.rose()) {
+    mainFsm.trigger(SAFETY_ON);
+  }
   if(countdown <= 0) {
     mainFsm.trigger(LAUNCH);
   }
 }
 
+void on_settingname_update(void *ctx) {
+  if(launchSwitch.rose()) {
+    currentSetting++;
+    on_settingname_enter(ctx);    
+  }
+  if(safetySwitch.rose()) {
+    if(strcmp(settings[currentSetting].Name, "Exit")==0) {
+      mainFsm.trigger(EXIT_SETTING);
+    } else {
+      mainFsm.trigger(ENTER_SETTING_VALUE);
+    }
+  }
+}
+void on_settingvalue_update(void *ctx) {
+  if(launchSwitch.rose()) {
+    pendingValues[currentSetting]++;
+    on_settingvalue_enter(ctx);    
+  }
+  if(safetySwitch.fell()) {
+    mainFsm.trigger(ENTER_SETTING);
+  }
+}
 
-State state_idle(on_idle_enter, nullptr, nullptr);
+
+
+void on_standard_update(void*){
+  if(safetySwitch.rose()) {
+    mainFsm.trigger(SAFETY_ON);
+  }
+  if(safetySwitch.fell()) {
+    mainFsm.trigger(SAFETY_OFF);
+  }
+  if(launchSwitch.fell()) {
+    mainFsm.trigger(LAUNCH_ON);
+  }
+  if(launchSwitch.rose()) {
+    mainFsm.trigger(LAUNCH_OFF);
+  }
+}
+
+void on_enter_setting(void*) {
+  memcpy(pendingValues, settingValues, SETTING_MAX * sizeof(uint8_t));
+}
+
+void on_exit_setting(void*) {
+  if(memcmp(pendingValues, settingValues, SETTING_MAX * sizeof(uint8_t)) != 0) {
+    Serial.println("Updating values");
+
+    memcpy(settingValues, pendingValues, SETTING_MAX * sizeof(uint8_t));
+    i2ceeprom.write(0, settingValues, sizeof(uint8_t) * SETTING_MAX);
+  }
+}
+
+State state_idle(on_idle_enter, on_standard_update, nullptr);
 
 State state_prearmed(on_prearm_enter, on_prearm, nullptr);
 
-State state_setting(on_setting_enter, nullptr, nullptr);
-State state_settingname(on_settingname_enter, nullptr, nullptr);
-State state_settingvalue(on_settingvalue_enter, nullptr, nullptr);
+State state_setting(on_setting_enter, on_standard_update, nullptr);
+State state_settingname(on_settingname_enter, on_settingname_update, nullptr);
+State state_settingvalue(on_settingvalue_enter, on_settingvalue_update, nullptr);
 
-State state_armed(on_armed_enter, nullptr, on_armed_exit);
+State state_armed(on_armed_enter, on_standard_update, on_armed_exit);
 
-State state_launching(on_Launching_enter, nullptr, on_launching_exit);
+State state_launching(on_Launching_enter, on_standard_update, on_launching_exit);
 
 State state_countdown(on_countdown_enter, on_countdown_update, nullptr );
-State state_abort(on_abort_enter, nullptr, nullptr);
+State state_abort(on_abort_enter, on_standard_update, nullptr);
 
-State state_postlaunch(on_postlaunch_enter, nullptr, nullptr);
+State state_postlaunch(on_postlaunch_enter, on_standard_update, nullptr);
 
 
 void setup() {
+
   Serial.begin(9600);
+
+  if (i2ceeprom.begin(0x50)) {  // you can stick the new i2c addr in here, e.g. begin(0x51);
+    Serial.println("Found I2C EEPROM");
+  } else {
+    Serial.println("I2C EEPROM not identified ... check your connections?\r\n");
+    while (1) delay(10);
+  }
+
+  i2ceeprom.read(0, settingValues, sizeof(uint8_t) * SETTING_MAX);
+  for(size_t i = 0; i < SETTING_MAX; ++i) {
+    if(settingValues[i] == 255) {
+      settingValues[i] = 0;
+    }
+  }
+
+  bool read(uint16_t addr, uint8_t *buffer, uint16_t num);
+  memset(settingValues, 0, SETTING_MAX * sizeof(uint8_t) );
+
 
   pinMode(FLASH_PIN, OUTPUT);
   pinMode(RELAY_PIN, OUTPUT);
@@ -288,196 +372,42 @@ void setup() {
 
   mainFsm.add_timed_transition(&state_init, &state_idle, 50, nullptr);
 
-  mainFsm.add_transition(&state_idle, &state_prearmed, ARMING_DOWN, nullptr);
+  mainFsm.add_transition(&state_idle, &state_prearmed, SAFETY_OFF, nullptr);
 
-  mainFsm.add_transition(&state_prearmed, &state_setting, ENTER_SETTING, nullptr);
+  mainFsm.add_timed_transition(&state_prearmed, &state_setting, 3000, nullptr);
   mainFsm.add_transition(&state_prearmed, &state_armed, ENTER_ARMED, nullptr);
 
-  mainFsm.add_transition(&state_setting, &state_idle, ARMING_UP, nullptr);
-  mainFsm.add_transition(&state_setting, &state_settingname, LAUNCH_UP, nullptr);
+  mainFsm.add_transition(&state_setting, &state_idle, SAFETY_ON, on_enter_setting);
+  mainFsm.add_transition(&state_setting, &state_settingname, LAUNCH_OFF, nullptr);
 
-  mainFsm.add_transition(&state_settingname, &state_settingvalue, ARMING_UP, nullptr);
-  mainFsm.add_transition(&state_settingname, &state_settingname, LAUNCH_DOWN, [](void *) { currentSetting++; } ); 
+  mainFsm.add_transition(&state_settingname, &state_settingvalue, ENTER_SETTING_VALUE, nullptr ); 
+  mainFsm.add_transition(&state_settingname, &state_idle, EXIT_SETTING, on_exit_setting);
 
-  mainFsm.add_transition(&state_settingvalue, &state_settingname, ARMING_DOWN, nullptr);
-  mainFsm.add_transition(&state_settingvalue, &state_settingvalue, LAUNCH_DOWN, [](void *) { currentValue++; } ); 
+  mainFsm.add_transition(&state_settingvalue, &state_settingname, ENTER_SETTING, nullptr);
 
-  mainFsm.add_transition(&state_countdown, &state_abort, ARMING_UP, [](void *) { lightFsm.trigger(LIGHT_STROBE);});
+  mainFsm.add_transition(&state_countdown, &state_abort, SAFETY_ON, [](void *) { lightFsm.trigger(LIGHT_STROBE);});
   mainFsm.add_transition(&state_countdown, &state_launching, LAUNCH, nullptr);
-
 
   mainFsm.add_timed_transition(&state_abort, &state_idle, 3000, nullptr);
 
-  mainFsm.add_transition(&state_armed, &state_countdown, LAUNCH_DOWN, nullptr);
-  mainFsm.add_transition(&state_armed, &state_idle, ARMING_UP, nullptr);
+  mainFsm.add_transition(&state_armed, &state_countdown, LAUNCH_ON, nullptr);
+  mainFsm.add_transition(&state_armed, &state_idle, SAFETY_ON, nullptr);
 
   mainFsm.add_timed_transition(&state_launching, &state_postlaunch, 300, nullptr);
-  mainFsm.add_transition(&state_postlaunch, &state_idle, ARMING_UP, nullptr);
+  mainFsm.add_transition(&state_postlaunch, &state_idle, SAFETY_ON, nullptr);
 
   
   // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  
-  display.display();
-  delay(1000);
+  // the library initializes this with an Adafruit splash screen.  
   display.clearDisplay();
   display.display();
-
-  // testdrawline();
-  // testdrawchar();
-  // testdrawstyles();
-  // testscrolltext();
 }
 
 void loop() {
   launchSwitch.update();
   safetySwitch.update();
 
-  if(launchSwitch.fell()) {
-    mainFsm.trigger(LAUNCH_DOWN);
-  }
-  
-  if(launchSwitch.rose()) {
-    mainFsm.trigger(LAUNCH_UP);
-  }
-
-  if(safetySwitch.fell()) {
-    mainFsm.trigger(ARMING_DOWN);
-  }
-
-  if(safetySwitch.rose()) {
-    mainFsm.trigger(ARMING_UP);
-  }
-  
-  // put your main code here, to run repeatedly:
-
   mainFsm.run_machine();
   lightFsm.run_machine();
 }
 
-
-void testdrawline() {
-  int16_t i;
-
-  display.clearDisplay(); // Clear display buffer
-
-  for(i=0; i<display.width(); i+=4) {
-    display.drawLine(0, 0, i, display.height()-1, SSD1306_WHITE);
-    display.display(); // Update screen with each newly-drawn line
-    delay(1);
-  }
-  for(i=0; i<display.height(); i+=4) {
-    display.drawLine(0, 0, display.width()-1, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-
-  for(i=0; i<display.width(); i+=4) {
-    display.drawLine(0, display.height()-1, i, 0, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  for(i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(0, display.height()-1, display.width()-1, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-
-  for(i=display.width()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, i, 0, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  for(i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, 0, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-
-  for(i=0; i<display.height(); i+=4) {
-    display.drawLine(display.width()-1, 0, 0, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  for(i=0; i<display.width(); i+=4) {
-    display.drawLine(display.width()-1, 0, i, display.height()-1, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000); // Pause for 2 seconds
-}
-
-void testdrawchar(void) {
-  display.clearDisplay();
-
-  display.setTextSize(1);      // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.setCursor(0, 0);     // Start at top-left corner
-  display.cp437(true);         // Use full 256 char 'Code Page 437' font
-
-  // Not all the characters will fit on the display. This is normal.
-  // Library will draw what it can and the rest will be clipped.
-  for(int16_t i=0; i<256; i++) {
-    if(i == '\n') display.write(' ');
-    else          display.write(i);
-  }
-
-  display.display();
-  delay(2000);
-}
-
-void testdrawstyles(void) {
-  display.clearDisplay();
-
-  display.setTextSize(1);             // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);        // Draw white text
-  display.setCursor(0,0);             // Start at top-left corner
-  display.println(F("Hello, world!"));
-
-  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
-  display.println(3.141592);
-
-  display.setTextSize(2);             // Draw 2X-scale text
-  display.setTextColor(SSD1306_WHITE);
-  display.print(F("0x")); display.println(0xDEADBEEF, HEX);
-
-  display.display();
-  delay(2000);
-}
-
-void testscrolltext(void) {
-  display.clearDisplay();
-
-  display.setTextSize(2); // Draw 2X-scale text
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(10, 0);
-  display.println(F("scroll"));
-  display.display();      // Show initial text
-  delay(100);
-
-  // Scroll in various directions, pausing in-between:
-  display.startscrollright(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrollleft(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrolldiagright(0x00, 0x07);
-  delay(2000);
-  display.startscrolldiagleft(0x00, 0x07);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-}
