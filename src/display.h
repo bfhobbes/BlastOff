6bc4d985e984f79aaf7b4f2758b722e923eab943
Andrew McDonald <47397734+bfhobbes@users.noreply.github.com>
2021-08-12 21:44:01 -0700

    Moving display methods out to separate file.

---
+++ src/display.h
@@ -0,0 +1,11 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+#include <Arduino.h>
+
+void displayInit();
+void clearDisplay();
+void showText(const arduino::__FlashStringHelper *text);
+void showCountdownText(const arduino::__FlashStringHelper *text);
+void showText3(const arduino::__FlashStringHelper *text1, const arduino::__FlashStringHelper *text2, const arduino::__FlashStringHelper *text3);
+
+#endif // DISPLAY_H
