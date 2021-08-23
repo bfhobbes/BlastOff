f8c65b4a2a3fce388bcd5722316ab1e8f23501d7
Andrew McDOnald <47397734+bfhobbes@users.noreply.github.com>
2021-08-23 13:47:39 -0700

    Adding cub log to startup For some reason couldn't have it in a separate file - linker issues. A problem to solve another day

--- src/display.h
+++ src/display.h
@@ -8,5 +8,6 @@ void clearDisplay();
 void showText(const arduino::__FlashStringHelper *text);
 void showCountdownText(const arduino::__FlashStringHelper *text);
 void showText3(const arduino::__FlashStringHelper *text1, const arduino::__FlashStringHelper *text2, const arduino::__FlashStringHelper *text3, int invertLine = -1);
+void showLogo();
 
 #endif // DISPLAY_H
